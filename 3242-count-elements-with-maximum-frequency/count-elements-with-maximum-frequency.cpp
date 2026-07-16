28f122d8ad3cc692fea0ebead82e5d4d2c0e36a9
Himanshi Agrawal <123625180+himanshiagrawal22@users.noreply.github.com>
2026-07-16 18:02:09 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (67.73%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -6,11 +6,11 @@ public:
             mp[x]++;
         }
         int maxFreq=0;
-        for(auto x:mp){
+        for(auto &x:mp){
             maxFreq=max(maxFreq,x.second);
         }
         int ans=0;
-        for(auto x:mp){
+        for(auto &x:mp){
             if(maxFreq==x.second){
                 ans+=x.second;
             }
