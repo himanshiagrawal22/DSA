80023abc9ba17db0f2c5634aa191295e7ea8eddf
Himanshi Agrawal <123625180+himanshiagrawal22@users.noreply.github.com>
2026-07-18 11:03:21 +0530

    Time: 4 ms (31.35%) | Memory: 8.6 MB (37.89%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -5,7 +5,7 @@ public:
         int num=x;
         long long rev=0;
         while(x>0){
-                long long rem=x%10;
+                int rem=x%10;
                 rev=rev*10+rem;
                 x=x/10;
         }
